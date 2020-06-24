df46698895c9fe535dc1eaae16f5037dd8fdfc9a
Thinegan Ratnam <thinegan@gmail.com>
2020-06-24 22:21:55 +0800

    VPC running service

--- timeclone1/modules/vpc/dev-vpc.tf
+++ timeclone1/modules/vpc/dev-vpc.tf
@@ -19,20 +19,8 @@ module "vpc" {
   private_subnets = ["10.38.48.0/20", "10.38.64.0/20", "10.38.80.0/20"]
 
   enable_ipv6 = true
-
-
-  # enable_nat_gateway = true
-  # single_nat_gateway = false
-
-  # # public_subnet_tags = {
-  # #   Name = "overridden-name-public"
-  # # }
-
-  # # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
-  # enable_flow_log                      = true
-  # create_flow_log_cloudwatch_log_group = true
-  # create_flow_log_cloudwatch_iam_role  = true
-  # flow_log_max_aggregation_interval    = 60
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   # Setup NACL - Public
   public_dedicated_network_acl = true
