7af2782995fa2e82afe79e0d74155ba79dd0cc35
Thinegan Ratnam <thinegan@gmail.com>
2020-09-05 22:03:57 +0800

    Fix added icmp for vpn

--- timeclone/network/vpn-endpoint/vpnendpoint.tf
+++ timeclone/network/vpn-endpoint/vpnendpoint.tf
@@ -84,9 +84,9 @@ resource "aws_security_group" "my_vpn_sg" {
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
