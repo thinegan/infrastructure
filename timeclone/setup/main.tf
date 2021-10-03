fadf44027e7d909494b1ce333bf612eb9c21763e
Thinegan Ratnam <thinegan@gmail.com>
2021-10-03 21:32:20 +0800

    Test deploy atlantis 1

--- timeclone/setup/main.tf
+++ timeclone/setup/main.tf
@@ -1,32 +1,81 @@
-// AWS Provider
+// Updates
 provider "aws" {
-  version  = "2.67"
-  region   = "us-east-1"
+  profile = "default"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-staging-state-storage"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
+terraform {
+  backend "s3" {
+    bucket         = "terraform-stag-state-storage"
+    key            = "global/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "stag-terraform-state-table"
   }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket" "bucket" {
+    bucket = "terraform-stag-state-storage"
+    versioning {
+        enabled = true
     }
-  }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-staging-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "stag-terraform-state-table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
 }
+
+# // AWS Provider
+# provider "aws" {
+#   version  = "2.67"
+#   region   = "us-east-1"
+# }
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-staging-state-storage"
+#   # Enable versioning so we can see the full revision history of our
+#   # state files
+#   versioning {
+#     enabled = true
+#   }
+#   # Enable server-side encryption by default
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
+
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-staging-state-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
